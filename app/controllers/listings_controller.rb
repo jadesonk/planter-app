1c80813caf0c6207fc8f60b07dff7ca48eb5e06c
jadeson komolvipat <jadesonk@gmail.com>
2020-09-22 18:23:45 +0700

    new + create for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,4 +5,24 @@ class ListingsController < ApplicationController
 
   def show
   end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+    if @listing.save
+      redirect_to @listing
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description)
+  end
 end
