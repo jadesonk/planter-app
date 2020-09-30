ac8611c20cc75240508fb7d2f9a0457d78742020
jadeson komolvipat <jadesonk@gmail.com>
2020-09-30 11:24:04 +0700

    display messages depending on conversation with stimulus

--- app/javascript/controllers/conversations_controller.js
+++ app/javascript/controllers/conversations_controller.js
@@ -2,11 +2,56 @@ import { Controller } from "stimulus";
 import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  // static targets = ['form', 'query', 'select', 'display', 'header'];
+  static targets = ['display'];
 
-  connect() {
-    console.log('hello');
+  connect() {}
+
+  select(e) {
+    e.preventDefault();
+    const tab = e.currentTarget;
+    const url = tab.getAttribute("href");
+    fetch(url, { headers: { accept: "application/json" } })
+      .then(response => response.json())
+      .then((data) => {
+        // console.log(data);
+        const currentUser = data.current_user.id;
+
+        let conversationContent = ""
+        data.messages.forEach((message) => {
+          const isCurrentUser = message.user_id === currentUser;
+          // console.log(this.createMessage(message.content, isCurrentUser));
+          conversationContent += this.createMessage(message.content, isCurrentUser);
+          // console.log(conversationContent);
+        })
+        console.log(conversationContent);
+        this.displayTarget.innerHTML = conversationContent;
+        console.log(this.displayTarget.innerHTML);
+      })
   }
+
+  createMessage(message, isCurrentUser) {
+    const messageClass = isCurrentUser ? 'message-sender' : 'message-receiver';
+    const messageHTML = `
+      <li class="message-content ${messageClass}">
+        <p>${message}<p>
+      </li>
+    `
+    return messageHTML;
+  }
+
+  result(e) {
+
+    // const data = e.detail[0].body.querySelector('.listing-list-container').innerHTML
+    // if (data.length > 0) {
+    //   return this.displayTarget.innerHTML = data
+    // }
+  }
+
+  error(e) {
+    console.log('error::', e);
+  }
+
+
 
   // search(e) {
   //   const query = this.queryTarget.value
@@ -18,14 +63,4 @@ export default class extends Controller {
   //   Rails.fire(this.formTarget, 'submit')
   // }
 
-  // result(e) {
-  //   const data = e.detail[0].body.querySelector('.listing-list-container').innerHTML
-  //   if (data.length > 0) {
-  //     return this.displayTarget.innerHTML = data
-  //   }
-  // }
-
-  // error(e) {
-  //   console.log('error::', e);
-  // }
 }
