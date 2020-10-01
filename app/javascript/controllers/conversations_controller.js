01cee624fc9eac1921dc30633510d666346ec04d
jadeson komolvipat <jadesonk@gmail.com>
2020-10-01 13:20:04 +0700

    complete messaging feature

--- app/javascript/controllers/conversations_controller.js
+++ app/javascript/controllers/conversations_controller.js
@@ -3,7 +3,7 @@ import Rails from "@rails/ujs"
 import { fetchWithToken } from "../utils/fetch_with_token";
 
 export default class extends Controller {
-  static targets = ['display', 'form', 'header', 'message'];
+  static targets = ['display', 'form', 'header', 'message', 'input'];
 
   connect() {}
 
@@ -11,24 +11,19 @@ export default class extends Controller {
     e.preventDefault();
     const tab = e.currentTarget;
     const url = tab.getAttribute("href");
+    this.inputTarget.classList.add('active');
+    this.displayTarget.innerHTML = ""
     fetch(url, { headers: { accept: "application/json" } })
       .then(response => response.json())
       .then((data) => {
         console.log(data);
         const currentUser = data.current_user.id;
-        let conversationContent = ""
-
         this.headerTarget.dataset.conversationId = data.conversation.id;
-
         data.messages.forEach((message) => {
           const isCurrentUser = message.user_id === currentUser;
-          // console.log(this.createMessage(message.content, isCurrentUser));
-          conversationContent += this.createMessage(message.content, isCurrentUser);
-          // console.log(conversationContent);
+
+          this.displayTarget.insertAdjacentHTML('afterbegin', this.createMessage(message.content, isCurrentUser))
         })
-        // console.log(conversationContent);
-        this.displayTarget.innerHTML = conversationContent;
-        // console.log(this.displayTarget.innerHTML);
       })
   }
 
@@ -59,48 +54,15 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         if (data.success) {
-          this.displayTarget.insertAdjacentHTML('beforeend', this.createMessage(data.message, true))
+          this.displayTarget.insertAdjacentHTML('afterbegin', this.createMessage(data.message, true))
           this.messageTarget.value = ""
         } else {
           console.log(data.errors);
         }
       })
-
-//       const searchAlgoliaPlaces = (event) => {
-//   fetch("https://places-dsn.algolia.net/1/places/query", {
-//     method: "POST",
-//     body: JSON.stringify({ query: event.currentTarget.value })
-//   })
-//     .then(response => response.json())
-//     .then((data) => {
-//       console.log(data.hits); // Look at local_names.default
-//     });
-// };
   }
 
-  result(e) {
-    const conversationId = this.headerTarget.dataset.conversationId;
-    const url = `/conversations/${conversationId}`;
-    // fetch(url, { headers: { accept: "application/json" } })
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     const currentUser = data.current_user.id;
-    //     let conversationContent = ""
-
-    //     // this.headerTarget.dataset.conversationId = data.conversation.id;
-
-    //     data.messages.forEach((message) => {
-    //       const isCurrentUser = message.user_id === currentUser;
-    //       // console.log(this.createMessage(message.content, isCurrentUser));
-    //       conversationContent += this.createMessage(message.content, isCurrentUser);
-    //       // console.log(conversationContent);
-    //     })
-    //     console.log(conversationContent);
-    //     this.displayTarget.innerHTML = conversationContent;
-    //     // console.log(this.displayTarget.innerHTML);
-    //   })
-  }
+  result(e) {}
 
   error(e) {
     console.log('error::', e);
