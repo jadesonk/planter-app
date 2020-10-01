9eafcf229fe7127877b9bc58001c75cc45e5ec80
Hui Ling, Jann <glis_sando88@yahoo.com.sg>
2020-10-01 15:31:35 +0800

    remove comment in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,94 +12,94 @@ require 'nokogiri'
 require_relative 'seed_data'
 require "pry-byebug"
 
-# puts "START SEED"
+puts "START SEED"
 
-# puts "Destroy All Users"
-# User.destroy_all
+puts "Destroy All Users"
+User.destroy_all
 
-# puts "Create Users"
+puts "Create Users"
 
-# User.create!(
-#   email: 'jade@test.com',
-#   password: '123123'
-# )
+User.create!(
+  email: 'jade@test.com',
+  password: '123123'
+)
 
-# 10.times do
-#   User.create!(
-#     email: Faker::Name.unique.first_name + '@test.com',
-#     password: '123123'
-#   )
-# end
+10.times do
+  User.create!(
+    email: Faker::Name.unique.first_name + '@test.com',
+    password: '123123'
+  )
+end
 
-# tags = ['sun requirement', 'edible', 'beginner friendly', 'low maintenance', 'high maintenance']
-# puts "Destroy All Tags"
-# Tag.destroy_all
+tags = ['sun requirement', 'edible', 'beginner friendly', 'low maintenance', 'high maintenance']
+puts "Destroy All Tags"
+Tag.destroy_all
 
-# puts
-# puts "Creating listing tags"
-# tags.each do |tag|
-#   Tag.create!(name: tag)
-#   puts "Created [#{tag}] Tag"
-# end
-# puts
+puts
+puts "Creating listing tags"
+tags.each do |tag|
+  Tag.create!(name: tag)
+  puts "Created [#{tag}] Tag"
+end
+puts
 
-# puts "Create Listings for users"
-# url = "https://plantswap.org/listings/page/4/"
-# html_file = open(url).read
-# html_doc = Nokogiri::HTML(html_file)
+puts "Create Listings for users"
+url = "https://plantswap.org/listings/page/4/"
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
 
-# html_doc.search('.geodir-category-list-view > li').each do |element|
-#   title = element.search('.geodir-entry-title a').text.strip
-#   href_url = element.search('.geodir-field-post_content a').attribute('href').value
-#   description_doc = Nokogiri::HTML(open(href_url).read)
-#   description = description_doc.search('.geodir-field-post_content p').text.strip
+html_doc.search('.geodir-category-list-view > li').each do |element|
+  title = element.search('.geodir-entry-title a').text.strip
+  href_url = element.search('.geodir-field-post_content a').attribute('href').value
+  description_doc = Nokogiri::HTML(open(href_url).read)
+  description = description_doc.search('.geodir-field-post_content p').text.strip
 
-#   listing_attr = {
-#     title: title,
-#     description: description,
-#     listing_type: (0..2).to_a.sample,
-#     status: 0,
-#     expiry_date: Date.today - 10 + (8..45).to_a.sample
-#   }
+  listing_attr = {
+    title: title,
+    description: description,
+    listing_type: (0..2).to_a.sample,
+    status: 0,
+    expiry_date: Date.today - 10 + (8..45).to_a.sample
+  }
 
-#   new_listing = Listing.new(listing_attr)
-#   # add random tags
-#   puts "randomizing tags for listings"
-#   (0..3).to_a.sample.times do
-#     random_tag = Tag.offset(rand(Tag.count)).first
-#     unless new_listing.tags.include? random_tag
-#       new_listing.tags << random_tag
-#       puts "added [#{random_tag.name}] to listing - #{title}"
-#     end
-#   end
+  new_listing = Listing.new(listing_attr)
+  # add random tags
+  puts "randomizing tags for listings"
+  (0..3).to_a.sample.times do
+    random_tag = Tag.offset(rand(Tag.count)).first
+    unless new_listing.tags.include? random_tag
+      new_listing.tags << random_tag
+      puts "added [#{random_tag.name}] to listing - #{title}"
+    end
+  end
 
-#   scrape and upload imgs to cloudinary for each listing
-#   puts "Add images to listing"
-#   img_url = "https://www.gardeneur.com/?category=all&transaction_type=all&view=list"
-#   img_doc = Nokogiri::HTML(open(img_url).read)
-#   (0..5).to_a.sample.times do
-#     random_img = img_doc.search('.home-list-image-container-desktop').to_a.sample
-#     random_img_url = random_img.children.attribute("src").value
-#     random_img_slug = random_img.attribute('href').value.match(/-(.*)/)[1]
-#     random_img_file = URI.open(random_img_url)
-#     new_listing.photos.attach(io: random_img_file, filename: "#{random_img_slug}.jpeg", content_type: 'image/jpeg')
-#   end
-#   puts "Add images to listing"
-#   photo_count = 0
-#   (3..5).to_a.sample.times do
-#     random_img_url = SeedData.get_img_thumbnails.sample
-#     random_img_file = URI.open(random_img_url)
-#     new_listing.photos.attach(io: random_img_file, filename: "planter-image-thumbnail-#{photo_count}.jpeg", content_type: 'image/jpeg')
-#     photo_count += 1
-#   end
+  scrape and upload imgs to cloudinary for each listing
+  puts "Add images to listing"
+  img_url = "https://www.gardeneur.com/?category=all&transaction_type=all&view=list"
+  img_doc = Nokogiri::HTML(open(img_url).read)
+  (0..5).to_a.sample.times do
+    random_img = img_doc.search('.home-list-image-container-desktop').to_a.sample
+    random_img_url = random_img.children.attribute("src").value
+    random_img_slug = random_img.attribute('href').value.match(/-(.*)/)[1]
+    random_img_file = URI.open(random_img_url)
+    new_listing.photos.attach(io: random_img_file, filename: "#{random_img_slug}.jpeg", content_type: 'image/jpeg')
+  end
+  puts "Add images to listing"
+  photo_count = 0
+  (3..5).to_a.sample.times do
+    random_img_url = SeedData.get_img_thumbnails.sample
+    random_img_file = URI.open(random_img_url)
+    new_listing.photos.attach(io: random_img_file, filename: "planter-image-thumbnail-#{photo_count}.jpeg", content_type: 'image/jpeg')
+    photo_count += 1
+  end
 
-#   # get a random user
-#   random_user = User.offset(rand(User.count)).first
-#   puts "assigning [#{random_user.email}] to listing - #{title}"
-#   # assign user to new listing
-#   new_listing.user = random_user
-#   new_listing.save
-# end
+  # get a random user
+  random_user = User.offset(rand(User.count)).first
+  puts "assigning [#{random_user.email}] to listing - #{title}"
+  # assign user to new listing
+  new_listing.user = random_user
+  new_listing.save
+end
 
 
 # create plant stories for users
