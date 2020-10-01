2b287d00a76d370a18130a327b3f3f40c99ec14f
jadeson komolvipat <jadesonk@gmail.com>
2020-10-01 11:53:24 +0700

    complete functionality for messaging

--- app/javascript/controllers/conversations_controller.js
+++ app/javascript/controllers/conversations_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from "stimulus";
 import Rails from "@rails/ujs"
+import { fetchWithToken } from "../utils/fetch_with_token";
 
 export default class extends Controller {
-  static targets = ['display'];
+  static targets = ['display', 'form', 'header', 'message'];
 
   connect() {}
 
@@ -13,19 +14,21 @@ export default class extends Controller {
     fetch(url, { headers: { accept: "application/json" } })
       .then(response => response.json())
       .then((data) => {
-        // console.log(data);
+        console.log(data);
         const currentUser = data.current_user.id;
-
         let conversationContent = ""
+
+        this.headerTarget.dataset.conversationId = data.conversation.id;
+
         data.messages.forEach((message) => {
           const isCurrentUser = message.user_id === currentUser;
           // console.log(this.createMessage(message.content, isCurrentUser));
           conversationContent += this.createMessage(message.content, isCurrentUser);
           // console.log(conversationContent);
         })
-        console.log(conversationContent);
+        // console.log(conversationContent);
         this.displayTarget.innerHTML = conversationContent;
-        console.log(this.displayTarget.innerHTML);
+        // console.log(this.displayTarget.innerHTML);
       })
   }
 
@@ -39,28 +42,67 @@ export default class extends Controller {
     return messageHTML;
   }
 
-  result(e) {
+  submit(e) {
+    e.preventDefault();
+    const conversationId = this.headerTarget.dataset.conversationId;
+    const url = `/conversations/${conversationId}/messages`;
+    this.formTarget.action = url;
+    // Rails.fire(this.formTarget, 'submit')
+    fetchWithToken(url, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ message: { content: this.messageTarget.value } })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.success) {
+          this.displayTarget.insertAdjacentHTML('beforeend', this.createMessage(data.message, true))
+          this.messageTarget.value = ""
+        } else {
+          console.log(data.errors);
+        }
+      })
 
-    // const data = e.detail[0].body.querySelector('.listing-list-container').innerHTML
-    // if (data.length > 0) {
-    //   return this.displayTarget.innerHTML = data
-    // }
+//       const searchAlgoliaPlaces = (event) => {
+//   fetch("https://places-dsn.algolia.net/1/places/query", {
+//     method: "POST",
+//     body: JSON.stringify({ query: event.currentTarget.value })
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data.hits); // Look at local_names.default
+//     });
+// };
+  }
+
+  result(e) {
+    const conversationId = this.headerTarget.dataset.conversationId;
+    const url = `/conversations/${conversationId}`;
+    // fetch(url, { headers: { accept: "application/json" } })
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     const currentUser = data.current_user.id;
+    //     let conversationContent = ""
+
+    //     // this.headerTarget.dataset.conversationId = data.conversation.id;
+
+    //     data.messages.forEach((message) => {
+    //       const isCurrentUser = message.user_id === currentUser;
+    //       // console.log(this.createMessage(message.content, isCurrentUser));
+    //       conversationContent += this.createMessage(message.content, isCurrentUser);
+    //       // console.log(conversationContent);
+    //     })
+    //     console.log(conversationContent);
+    //     this.displayTarget.innerHTML = conversationContent;
+    //     // console.log(this.displayTarget.innerHTML);
+    //   })
   }
 
   error(e) {
     console.log('error::', e);
   }
-
-
-
-  // search(e) {
-  //   const query = this.queryTarget.value
-  //   Rails.fire(this.formTarget, 'submit')
-  // }
-
-  // filter(e) {
-  //   const select = this.selectTarget.value
-  //   Rails.fire(this.formTarget, 'submit')
-  // }
-
 }
